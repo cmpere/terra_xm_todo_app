29eb3554c9d6a795a0c456d503e34cbe696e9193
Carlos Cruz <cmpere@gmail.com>
2025-11-23 01:49:38 -0600

    fix: Dinamic height calculation of todo list

--- resources/partials/list.php
+++ resources/partials/list.php
@@ -1,4 +1,4 @@
-<div class="max-h-[55vh] overflow-y-scroll" id="todo-list">
+<div class="max-h-[calc(100vh-(var(--spacing)*65))] overflow-y-scroll" id="todo-list">
     <div class="p-4 flex item-center justify-center">
         <i data-lucide="loader-circle" class="text-slate-900 animate-spin"></i>
     </div>
