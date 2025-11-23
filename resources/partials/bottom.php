783df40312eb50d7ccc342990de58e17c829d485
Carlos Cruz <cmpere@gmail.com>
2025-11-22 23:24:54 -0600

    fix: Added confirm modal and lucide icons create function

---
+++ resources/partials/bottom.php
@@ -0,0 +1,8 @@
+<div id="destroy-confirm" title="¿Deseas continuar?">
+  <p>Eliminar la tarea: <span id="destroy-confirm-message" class="font-bold"></span></p>   
+</div>
+
+<script src="https://unpkg.com/lucide@latest"></script>
+<script>
+  lucide.createIcons();
+</script>
