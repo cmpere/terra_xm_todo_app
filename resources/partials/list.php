0d6ce25ec27eedf61738a0e9af474fa0ffa35914
Carlos Cruz <cmpere@gmail.com>
2025-11-23 00:59:44 -0600

    fix: Missed list imported file

---
+++ resources/partials/list.php
@@ -0,0 +1,5 @@
+<div class="max-h-[55vh] overflow-y-scroll" id="todo-list">
+    <div class="p-4 flex item-center justify-center">
+        <i data-lucide="loader-circle" class="text-slate-900 animate-spin"></i>
+    </div>
+</div>
